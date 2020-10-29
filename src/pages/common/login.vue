0b07cff1707f139b3f315fbeb1c75e243e4f7f02
qq2316693281 <qq2316693281@sina.com>
2020-10-29 18:24:45 +0800

    1

--- src/pages/common/login.vue
+++ src/pages/common/login.vue
@@ -11,10 +11,29 @@ export default {
 </script>
 
 <style lang="less" scoped>
+// .login-container {
+//     // height: 100%;
+//     // min-height: 100vh;
+//     // background-position: center;
+//     // background-repeat: no-repeat;
+//     // background-attachment: fixed;\
+//     // background-image: url("~@/assets/image/login-bg.jpg");
+// }
 .login-container {
+    width: 100%;
     height: 100%;
     min-height: 100vh;
-    background-image: url(https://gw.alipayobjects.com/zos/rmsportal/TVYTbAXWheQpRcWDaDMu.svg);
-    background-size: cover;
+    background: url("~@/assets/image/login-bg.jpg") no-repeat;
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+}
+@media (max-width: 1200px) {
+    .login-container {
+        background-position: center;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size:;
+        background-image: url("~@/assets/image/login-bg.jpg");
+    }
 }
 </style>
